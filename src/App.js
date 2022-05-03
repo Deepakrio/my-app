5007862122dc0961f753dfb7118a6f24d1fe7443
deepakrio <deepakrio64@gmail.com>
2022-05-03 12:49:56 +0530

    commit react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route ,Link, Routes} from "react-router-dom";
+import About from"./components/page1" ;
+import Page2 from"./components/page2" ;
+import Table from"./components/page3" ;
+import React, {useState} from 'react';
+
+
+
 
 function App() {
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <div className="list">
+  <ul>
+    <li><Link to="/">Home</Link></li>
+    <li><Link to="About">About</Link></li>
+    <li><Link to="page2">Page2</Link></li>
+    <li><Link to="Table">Details</Link></li>
+
+  </ul>
+</div>
+  <Routes>
+  <Route exact path="/" element={<h1>Welcome To React</h1>} />
+    <Route exact path="About" element={<About title = 'Declarative' title2 = 'Component-Based' />} />
+    <Route exact path="page2" element={<Page2 />} />
+    <Route exact path="Table" element={<Table />} />
+
+  </Routes>
+
+ 
+</Router>
     </div>
   );
 }
+
+
 
 export default App;
